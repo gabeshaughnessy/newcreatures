a69ffbbdaacb2484370540a7dd24bf46b3033af3
gabeshaughnessy <gabeshaughnessy@gmail.com>
2013-05-17 12:35:17 -0700

    updated localscroll and scrollto for new version of jquery

--- wp-content/themes/newcreature-genesis-child/scripts/slider_box.js
+++ wp-content/themes/newcreature-genesis-child/scripts/slider_box.js
@@ -1,12 +1,16 @@
 jQuery(document).ready(function($){
-$(window).load(function(){
-$('#project_slider').cycle({ 
+jQuery('#wrap').localScroll();
+});
+
+jQuery(window).load(function(){
+jQuery('#project_slider').cycle({ 
     fx:'fade', 
     speed: 2000, 
         timeout: 5000, 
         containerResize: 1,
         pager:  '#project_pager', 
                  });
-                   $('#wrap').localScroll();
+                   //jQuery('#wrap').localScroll();
                  });
-                 });
+
+
